782b041c8079ae71b5b94ecb4cf3234814c50502
Adrian Romero <radrian031613@gmail.com>
2020-04-02 11:39:38 -0700

    first init and created mySql folder with set up for table in the employee database.

---
+++ mySql/employee_db.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department (
+id INTEGER AUTO_INCREMENT NOT NULL ,
+name VARCHAR(30) NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+id INTEGER NOT NULL AUTO_INCREMENT,
+title VARCHAR (30) NULL,
+salary DECIMAL (10,2) NULL,
+department_id INT (10) NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+id INTEGER NOT NULL AUTO_INCREMENT,
+first_name VARCHAR (30) NULL,
+last_name VARCHAR (30) NULL,
+role_id INT (30) NULL,
+manager_id INT (30) NULL,
+PRIMARY KEY (id)
+);
