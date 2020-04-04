6e8995ee81df7dcb4665fb70808f196198d4a094
Adrian Romero <radrian031613@gmail.com>
2020-04-03 22:05:14 -0700

    executeSearch function runs properly in the console. user is prompted and an aswer is console.logged to test functionality.

--- server.js
+++ server.js
@@ -4,88 +4,142 @@ const consoleTable = require('console.table');
 const mysql = require('mysql');
 
 // create server connection for use of mysql
-const connection = mysql.createConnecction({
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
-    database: ''
+    database: 'employee_db'
 });
 
 // function to show error if connection not made
 connection.connect(function (err) {
     if(err) throw err;
-    
+    executeSearch();
 })
 
 // write function to run search based on parameters input
 // use inquirer to prompt in console for user to make choice
 function executeSearch() {
-    
-}
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'action',
+            message: "What would you like to do?",
+            choices: ["View all employees",
+                "View all departments",
+                "View all managers",
+                "Add Employee",
+                "Add Department",
+                "Add Role",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "exit"
+            ]})
+            // write .then function to allow user to select subcategories based on selected choices
+            .then(function (answer) {
+                console.log(answer.action);
+                switch (answer.action) {
+                    case "View all employees":
+                        viewEmployees();
+                    break;
 
-// write .then function to allow user to select subcategories based on selected choices
+                    case "View all departments":
+                        viewDepartment();
+                    break;
+                    
+                    case "View all managers":
+                        viewManager();
+                    break;
 
+                    case "Add Employee":
+                        addEmployee();
+                    break;
 
+                    case "Add Department":
+                        addDepartment();
+                    break;
+
+                    case "Add Role":
+                        addRole();
+                    break;
+
+                    case "Remove Employee":
+                        removeEmployee();
+                    break;
+
+                    case "Update Employee Role":
+                        updateEmployee();
+                    break;
+
+                    case "exit":
+                        connection.end();
+                    break;
+                }
+            });
+            
+        }
+        
 // create function for viewing employees
-function viewEmployee() {
+// function viewEmployee() {
     
 
-}
-// create funciton to view departments available
-// let users view table of selected department
+// }
+// // create funciton to view departments available
+// // let users view table of selected department
 
-function viewDepartment() {
+// function viewDepartment() {
 
-}
+// }
 
-// create function to view managers available
+// // create function to view managers available
 
-function viewManager() {
+// function viewManager() {
 
-}
+// }
 
-// create function to add employee
-function addEmployee() {
-    // prompt user what table the new employee should be added to 
-    // use user input to make push into table
-}
+// // create function to add employee
+// function addEmployee() {
+//     // prompt user what table the new employee should be added to 
+//     // use user input to make push into table
+// }
 
-//create function to add department to the database
-function addDepartment() {
+// //create function to add department to the database
+// function addDepartment() {
 
-}
+// }
 
-// create function to add role
-function addRole() {
-    // prompt user what the new role is
-    // create then function to use input to create variable with the answer
+// // create function to add role
+// function addRole() {
+//     // prompt user what the new role is
+//     // create then function to use input to create variable with the answer
 
-    // prompt user to input salary for new role
-    // create then function to use input as new salary
+//     // prompt user to input salary for new role
+//     // create then function to use input as new salary
 
-    // prompt user to input new id for the role
-    // then function to create id and use in database
-}
+//     // prompt user to input new id for the role
+//     // then function to create id and use in database
+// }
 
-// create function to remove employee
-function removeEmployee() {
-    // prompt to remove employee by id
-    // then function to delete employee from database using answer
+// // create function to remove employee
+// function removeEmployee() {
+//     // prompt to remove employee by id
+//     // then function to delete employee from database using answer
 
-}
+// }
 
-// function to update existing employees
-function updateEmployee() {
-    // use inquirer to prompt user to enter employee id being updated
-    // then function to create id variable
+// // function to update existing employees
+// function updateEmployee() {
+//     // use inquirer to prompt user to enter employee id being updated
+//     // then function to create id variable
 
-    // inquirer promptto ask about role id
-    // then function to update role id
-}
+//     // inquirer promptto ask about role id
+//     // then function to update role id
+// }
 
 
-// create function to update an employee's manager
-function employeeManager() {
-    // inquirer prompt to ask what employee would the user like to update the manager
-    // then function to use answer and change info in mysql table
-}
+// // create function to update an employee's manager
+// function employeeManager() {
+//     // inquirer prompt to ask what employee would the user like to update the manager
+//     // then function to use answer and change info in mysql table
+// }
